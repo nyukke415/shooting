7a0e10efe830651abcf3e293a7a102d12048e7be
nyukke415 <nyukke415@gmail.com>
2019-06-21 16:35:47 +0900

    First commit.

--- javap_mod/go.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-if [ "$1" = "-B" ]; then
-  echo "compile && view"
-  javac SampleApplet.java && appletviewer SampleApplet.java
-else
-  appletviewer SampleApplet.java
-fi
-
--- javap_mod/shooting/go.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-if [ "$1" = "-B" ]; then
-  echo "compile && view"
-  javac Shooting.java && appletviewer Shooting.java
-else
-  appletviewer Shooting.java
-fi
-
